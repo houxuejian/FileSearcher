a179731b48fcc22ea103f3df3b283b73953d71b0
侯雪健 <houxuejian@gyyx.cn>
2017-01-09 02:38:12 +0800

    增加不包含文件名过滤器

--- src/main/java/utils/OnceSearchMainRun.java
+++ src/main/java/utils/OnceSearchMainRun.java
@@ -14,6 +14,7 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOCase;
 import org.apache.commons.io.filefilter.AndFileFilter;
 import org.apache.commons.io.filefilter.IOFileFilter;
+import org.apache.commons.io.filefilter.NotFileFilter;
 import org.apache.commons.io.filefilter.RegexFileFilter;
 import org.apache.commons.io.filefilter.SizeFileFilter;
 import org.apache.commons.io.filefilter.TrueFileFilter;
@@ -21,23 +22,30 @@ import org.apache.commons.io.filefilter.TrueFileFilter;
 public class OnceSearchMainRun {
 	
 	public OnceSearchMainRun() throws InterruptedException, ExecutionException {
-		String dir = "D:/gitlib";
+		String dir = "D:\\java5-gm\\universal_etl\\trunk\\script";
 		
-		SizeFileFilter sizeFileFilter = new SizeFileFilter((long) (0.5 * FileUtils.ONE_MB), false);
+		SizeFileFilter sizeFileFilter = new SizeFileFilter((long) (2 * FileUtils.ONE_MB), false);
 //		SizeFileFilter sizeFileFilter = new SizeFileFilter(2 * FileUtils.ONE_MB, false);
-		RegexFileFilter regexFileFilter = new RegexFileFilter(".*YARNRunner.*", IOCase.INSENSITIVE);
+		
+//		RegexFileFilter regexFileFilter = new RegexFileFilter(".*HiveDriver.java.*", IOCase.INSENSITIVE);
+		
+		NotFileFilter regexFileFilterNot = new NotFileFilter(new RegexFileFilter(".*Runtime.*", IOCase.INSENSITIVE));
+		NotFileFilter regexFileFilterNot2 = new NotFileFilter(new RegexFileFilter(".*ThinkPHP.*", IOCase.INSENSITIVE));
+		
 		
 		List<IOFileFilter> fileFilterList = new ArrayList<>();
 		fileFilterList.add(TrueFileFilter.INSTANCE);
 		fileFilterList.add(sizeFileFilter);
+//		fileFilterList.add(regexFileFilterNot);
 //		fileFilterList.add(regexFileFilter);
 		
-		IOFileFilter dirFilter = new AndFileFilter(TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
+		IOFileFilter dirFilter = new AndFileFilter(regexFileFilterNot, regexFileFilterNot2);
 		IOFileFilter fileFilter = new AndFileFilter(fileFilterList);
 		
 		
 		long t = System.currentTimeMillis();
-		Collection<File> files = FilePathSearcher.INSTANCE.getFileList(new File(dir), fileFilter, TrueFileFilter.INSTANCE, 10000);
+		Collection<File> files = FilePathSearcher.INSTANCE.getFileList(new File(dir), fileFilter, dirFilter, 10000);
+//		Collection<File> files = FilePathSearcher.INSTANCE.getFileList(new File(dir), fileFilter, TrueFileFilter.INSTANCE, 10000);
 //		Collection<File> files = getFileAndDirList(new File("D:/myJar"), fileFilter, TrueFileFilter.INSTANCE, 1);
 		
 		/*for (File file : files) {
@@ -47,7 +55,7 @@ public class OnceSearchMainRun {
 		if (files.isEmpty()) {
 			return;
 		}
-		List<File> files2 = FileContentSearcher.INSTANCE.parallelSearch(files, "setIp_white_list", false, Charset.forName("UTF-8"));
+		List<File> files2 = FileContentSearcher.INSTANCE.parallelSearch(files, "update_cm_start_inc", false, Charset.forName("UTF-8"));
 		System.out.println("time cost: " + (System.currentTimeMillis() - t));
 		
 		System.out.println(files2.size());
